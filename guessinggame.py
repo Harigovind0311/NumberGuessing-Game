c7c3ef68f43b8e77fd9adbf28eba0659ae5b9fff
Harigovind0311 <61371772+Harigovind0311@users.noreply.github.com>
2021-05-10 13:42:07 +0530

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,13 @@
+import dropbox
+
+while chances < 5
+
+print = int(input('Enter the correct number :  '))
+Enter the correct number: 6
+
+if guess ==number :
+    print(Congratulations you won!)
+    break
+
+if not chances <5:
+    print(You Lose)
